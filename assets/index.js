08d01ef3efe03444e78ced2ed0c1e6603f94d29f
Kangonga <guchu413@gmail.com>
2022-08-30 09:00:02 +0300

    change endpoint

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,80 @@
 document.addEventListener('DOMContentLoaded', fetchLink)
-
 function fetchLink(){
-    return(fetch(`https://short-link-api.vercel.app/?query=${link}`,{
-        method:'GET',
-        mode:'cors',
+    
+    return(fetch("https://gotiny.cc/api",
+    {
+        method:'POST',
         headers:{
-            'Access-Control-Allow-Origin':'*'
-        }
-    })
+            "Content-Type": "application/json" 
+        },
+        body:JSON.stringify({
+            // input:"https://jobs.gorails.com/",
+            long:"https://www.rubyjobboard.com/",
+            custom:"abcd",
+        
+        })
+    }))
     .then(resp=>resp.json())
-    .then(data=>console.log(data))
-    )
+    .then(data=>{
+        console.log(data)
+        console.log("gotiny.cc/"+data[0].code)
+    })
 }
-console.log("hello")
-let link = "https://jobs.gorails.com/"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
